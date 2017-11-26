c23fe7f0436d705da462bbcc50d13eeadca37806
Jenny-Le <lejenny75@gmail.com>
2017-11-26 11:49:49 -0700

    thirdpush

--- assets/javascript/questionpage.js
+++ assets/javascript/questionpage.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 	var itch = "";
 	var holiday = "";
 	var collection = "";
+	var correctAnswers = 0;
+	var inCorrectAnswers = 0;
+	var unAnswered = 0;
 	//attached click handler to input radio button
 	$("input[type=radio]").click(function(){
 		var formName = $(this).closest("form").attr('name')
@@ -26,11 +29,67 @@ $(document).ready(function() {
      	console.log(formName);
   	});
 
-  	var count = 30
+  	var count = 10
   	timer = setInterval(function() {
     	$("#counter").html(count--);
-    	if(count == -1) clearInterval(timer);
+    	if(count == -1){
+    		clearInterval(timer);
+
+    		if(location == "New York"){
+    			correctAnswers++
+    			// else statements do not need parenthasis ()
+    			// the statement needs to be in brakets
+			} else if(location == "") {
+				unAnswered++
+			} else {
+				inCorrectAnswers++
+			};
+
+			if(toy == "Coochie Kopi"){
+				correctAnswers++
+			} else{
+				inCorrectAnswers++
+			};
+			if(itch == "Her crotch"){
+				correctAnswers++
+			} else{
+				inCorrectAnswers++
+			}
+			if(holiday == "Thanksgiving"){
+				correctAnswers++
+			} else{
+				inCorrectAnswers++
+			}
+			if(collection == "Porcelain babies"){
+				correctAnswers++
+			} else{
+				inCorrectAnswers++
+			}
+			console.log('correct', correctAnswers);
+			console.log('incorrect', inCorrectAnswers);
+			console.log('unanswerd', unAnswered);
+
+			var resultString = ""
+			resultString += "<h2> Correct Answers: " + correctAnswers + "</h2>"
+			resultString += "<h2> Incorrect Answers: " + inCorrectAnswers + "</h2>"
+			resultString += "<h2> Unanswered: " + unAnswered + "</h2>"
+
+			$( "body" ).replaceWith(resultString);
+			
+
+
+
+
+    	};
 	}, 1000);
 })
 
+
+
+
+
+
+		
+
+	
  	
